a5f69527994834c9cecb97fdc070dae8a91a2bd5
vigorosodedeus@gmail.com <MoraisG>
2020-12-17 15:57:27 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/moraisg/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moraisg.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
